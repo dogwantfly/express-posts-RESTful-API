7424660d666e511e64e3acb8ec648bd6bd5a8761
hsin yu <tina2793778@gmail.com>
2024-04-30 17:14:57 +0800

    refactor: 💡 調整 var 宣告

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const cors = require("cors");
 
 
@@ -22,7 +22,7 @@ mongoose.connect(DB)
 
 const postsRouter = require('./routes/posts');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
