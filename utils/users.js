ab77b1b2a7cdfca4907d35f74810b7804005fa7f
hsin yu <tina2793778@gmail.com>
2024-05-18 15:42:44 +0800

    refactor: 💡 加入密碼驗證規則

--- utils/users.js
+++ utils/users.js
@@ -5,6 +5,9 @@ function validatePassword(password) {
   if (!validator.isLength(password, { min: 8 })) {
     throw new appError(400, '密碼字數低於 8 碼');
   }
+  if (!/[a-zA-Z]/.test(password) || !/[0-9]/.test(password)) {
+    throw new appError(400, '密碼需包含英文與數字混合');
+  }
 }
 
 const isSexInEnum = (value, enumArray) => {
